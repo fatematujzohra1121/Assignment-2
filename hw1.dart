f77cd0f62cf908ea20e674ad65f892ebb18c18bd
fatematujzohra1121 <113527306+fatematujzohra1121@users.noreply.github.com>
2022-12-21 21:06:11 +0600

    Add files via upload

---
+++ hw1.dart
@@ -0,0 +1,41 @@
+
+
+
+void main()
+{
+
+String letter = ('A+' ) ;
+
+switch (letter)
+{
+case 'A+':
+   print ('GPA 5');
+   break;
+case 'A':
+   print ('GPA 4');
+   break;
+
+case 'A-':
+   print ('GPA 3.5');
+   break;
+
+case 'B':
+   print ('GPA 3');
+   break;
+
+case 'C':
+   print ('GPA 2');
+   case 'D':
+   print ('GPA 1');
+   break;
+default:
+   print ('fail');
+
+
+}
+
+
+
+
+
+}
---
+++ hw2.dart
@@ -0,0 +1,12 @@
+
+
+void main()
+{
+  int i=0;
+  do
+    {
+       print ('Dart');
+       i++;
+    }
+  while (i < 17) ;  
+}
